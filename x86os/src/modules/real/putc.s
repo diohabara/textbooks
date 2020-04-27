d5fe4df30a87d66d9cc973fbc5cdb4e41ac24e42
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2020-04-27 23:23:16 +0900

    done 04

--- x86os/src/modules/real/putc.s
+++ x86os/src/modules/real/putc.s
@@ -1,1 +1,24 @@
 putc:
+    ; construction of stack
+    push bp
+    mov bp, sp
+
+    ; saving register
+    push ax
+    push bx
+
+    ; start of process
+    mov al, [bp + 4]
+    mov ah, 0x0E
+    mov bx, 0x0000
+    int 0x10
+
+    ; return to register
+    pop bx
+    pop ax
+
+    ; destruction of stack frame
+    mov sp, bp
+    pop bp
+
+    ret
