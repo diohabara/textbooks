66d5f2ac39bb0d1973d4073c07c9570604b12afb
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2020-04-30 22:07:53 +0900

    done 18

---
+++ x86os/src/18_draw_char/kernel.s
@@ -0,0 +1,37 @@
+%include "../include/define.s"
+%include "../include/macro.s"
+
+ORG KERNEL_LOAD
+
+[BITS  32]
+;; entry point
+kernel:
+  ; get font address
+  mov esi, BOOT_LOAD + SECT_SIZE ; ESI = 0x7C00
+  movzx eax, word [esi + 0]      ; EAX = [ESI + 0] // segment
+  movzx ebx, word [esi + 2]      ; EBX = [ESI + 2] // offset
+  shl eax, 4                     ; EAX <<= 4
+  add eax, ebx                   ; EAX += EBX
+  mov [FONT_ADR], eax            ; FONT[0] = EAX
+
+  ; display character
+  cdecl draw_char, 0, 0, 0x010F, 'A'
+  cdecl draw_char, 1, 0, 0x010F, 'B'
+  cdecl draw_char, 2, 0, 0x010F, 'C'
+
+  cdecl draw_char, 0, 0, 0x0402, '0'
+  cdecl draw_char, 1, 0, 0x0212, '1'
+  cdecl draw_char, 2, 0, 0x0212, '_'
+
+  ; end process
+  jmp $
+
+;; modules
+%include "../modules/protect/vga.s"
+%include "../modules/protect/draw_char.s"
+
+ALIGN 4, db 0
+FONT_ADR: dd 0
+
+;; padding
+  times KERNEL_SIZE - ($ - $$) db 0
---
+++ x86os/src/modules/protect/vga.s
@@ -0,0 +1,138 @@
+vga_set_read_plane:
+  push ebp
+  mov ebp, esp
+
+  push eax
+  push edx
+
+  mov ah, [ebp + 8]
+  and ah, 0x03
+  mov al, 0x04
+  mov dx, 0x03CE
+  out dx, ax
+
+  pop edx
+  pop eax
+
+  mov esp, ebp
+  pop ebp
+
+  ret
+
+vga_set_write_plane:
+  push ebp
+  mov ebp, esp
+
+  push eax
+  push edx
+
+  mov ah, [ebp + 8]
+  and ah, 0x0F
+  mov al, 0x02
+  mov dx, 0x03C4
+  out dx, ax
+
+  pop edx
+  pop eax
+
+  mov esp, ebp
+  pop ebp
+
+  ret
+
+vram_font_copy:
+  push ebp
+  mov ebp, esp
+
+  push eax
+  push ebx
+  push ecx
+  push edx
+  push esi
+  push edi
+
+  mov esi, [ebp + 8]
+  mov edi, [ebp + 12]
+  movzx eax, byte [ebp + 16]
+  movzx ebx, word [ebp + 20]
+
+  test bh, al
+  setz dh
+  dec dh
+
+  test bl, al
+  setz dl
+  dec dl
+
+  cld
+
+  mov ecx, 16
+  .10L:
+
+  lodsb
+  mov ah, al
+  not ah
+
+  and al, dl
+
+  test ebx, 0x0010
+  jz .11F
+  and ah, [edi]
+  jmp .11E
+  .11F:
+  and ah, dh
+  .11E:
+
+  or al, ah
+
+  mov [edi], al
+
+  add edi, 80
+  loop .10L
+  .10E:
+
+  pop edi
+  pop esi
+  pop edx
+  pop ecx
+  pop ebx
+  pop eax
+
+  mov esp, ebp
+  pop ebp
+
+  ret
+
+vram_bit_copy:
+  push ebp
+  mov ebp, esp
+
+  push eax
+  push ebx
+  push edi
+
+  mov edi, [ebp + 12]
+  movzx eax, byte [ebp + 16]
+  movzx ebx, word [ebp + 20]
+
+  test bl, al
+  setz bl
+  dec bl
+
+  mov al, [ebp + 8]
+  mov ah, al
+  not ah
+
+  and ah, [edi]
+  and al, bl
+  or al, ah
+  mov [edi], al
+
+  pop edi
+  pop ebx
+  pop eax
+
+  mov esp, ebp
+  pop ebp
+
+  ret
